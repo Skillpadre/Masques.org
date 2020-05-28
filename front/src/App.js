e481dae015839a9b06441ee0564bc4367e02c009
Christophe Applanat <c.applanat@gmail.com>
2020-05-28 16:13:58 +0200

    test front

--- front/src/App.js
+++ front/src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        hello world
         <a
           className="App-link"
           href="https://reactjs.org"

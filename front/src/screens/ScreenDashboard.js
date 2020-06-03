d5afb700b8d98274c7a886f333062e92188228fd
Jérémy <potel.jeremy@gmail.com>
2020-06-03 12:03:20 +0200

    front 2

--- front/src/screens/ScreenDashboard.js
+++ front/src/screens/ScreenDashboard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../App.css';
-import { Row, Col, Card, Button, Layout, List, Avatar} from 'antd';
+import { Row, Col, Card, Button, Layout, List, Avatar } from 'antd';
 import { Redirect, Link } from 'react-router-dom';
 import 'antd/dist/antd.css';
 import Nav from './Nav'
@@ -30,40 +30,56 @@ function ScreenDashboard() {
         <Row>
           <Col span={8}>
             <h1>Dashboard</h1>
-            </Col>
-            <Col span={6}>
-       <h2>Bienvenue John !</h2>
-       </Col>
-       <Col offset={4} span={6}>
-       <Button><Link to='/screenmap'>Passer une commande</Link></Button>
-       </Col>
-          
-       </Row>
-       <Row>
-       <Col span={10}>
-        <h2>Commandes en attente de validation</h2>
-        <div id="dashboard-box">
-          
-        <List
-    itemLayout="horizontal"
-    dataSource={data}
-    renderItem={item => (
-      <List.Item>
-        <List.Item.Meta
-          avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-          title={<a href="https://ant.design">{item.title}</a>}
-          description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-        />
-      </List.Item>
-    )}
-  />,
+          </Col>
+          <Col span={6}>
+            <h2>Bienvenue John !</h2>
+          </Col>
+          <Col offset={4} span={6}>
+            <Button><Link to='/screenmap'>Passer une commande</Link></Button>
+          </Col>
+
+        </Row>
+        <Row>
+          <Col span={10}>
+            <h2>Commandes en attente de validation</h2>
+            <div id="dashboard-box">
+
+              <List
+                itemLayout="horizontal"
+                dataSource={data}
+                renderItem={item => (
+                  <List.Item>
+                    <List.Item.Meta
+                      avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                      title={<a href="https://ant.design">{item.title}</a>}
+                      description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                    />
+                  </List.Item>
+                )}
+              />,
   </div>
-      </Col>
-       <Col span={10}>
-        <h2>Historique des commandes</h2>
-      </Col>
-       </Row>
-       
+          </Col>
+          <Col span={10}>
+            <h2>Historique des commandes</h2>
+            <div id="dashboard-box">
+
+              <List
+                itemLayout="horizontal"
+                dataSource={data}
+                renderItem={item => (
+                  <List.Item>
+                    <List.Item.Meta
+                      avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                      title={<a href="https://ant.design">{item.title}</a>}
+                      description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                    />
+                  </List.Item>
+                )}
+              />,
+  </div>
+          </Col>
+        </Row>
+
 
 
       </div>

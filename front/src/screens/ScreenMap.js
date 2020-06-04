739f7257ef5a76752c3f2ebf35e1abab5fc80bd8
Loëvane Vircondelet <loevane.vircondelet@gmail.com>
2020-06-04 17:43:29 +0200

    css

--- front/src/screens/ScreenMap.js
+++ front/src/screens/ScreenMap.js
@@ -19,14 +19,14 @@ const [zoom, setZoom] = useState(11);
 
       <div className="Map" > 
       <Button style={{backgroundColor : '#E23D70', borderRadius: 5, boxShadow: '0px 3px 3px 0px black'}}>Chercher les fabricants autour de moi</Button>
-      <div style={{ height: '50vh', width: '50%' }}>
+      <div style={{ height: '50vh', width: '50%', marginTop: 30}}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyA6lFML5Gv6tvWgNl0X7kXn6X1uMQyzX8o' }}
           defaultCenter={center}
           defaultZoom={zoom}
         
         />
-</div>
+      </div>
       </div>
     </div>
   );
